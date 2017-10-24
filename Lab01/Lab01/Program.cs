633d2b56878b24aeb1931e2f12ca97f6ded5f691
Woraprad <58030217@kmitl.ac.th>
2017-10-24 23:12:00 +0700

    Cos wave

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,8 +5,8 @@ public class MathTest
     {
         for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
         {
-            Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" +
-            spaces(Math.Sin(i)) + "*", i, Math.Sin(i));
+            Console.WriteLine("The Cos of {0,10:F} = {1,-10:F6}" +
+            spaces(Math.Cos(i)) + "*", i, Math.Cos(i));
         }
     }
     private static string spaces(double val)
