4ef2e937b1fb70737ae76222c0f1cbdc61d129e9
Woraprad <58030217@kmitl.ac.th>
2017-10-24 22:21:25 +0700

    SunToEarth

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-public class intergerTest
+namespace variableProperties
 {
-    static void Main(string[] args)
+    class Program
     {
-        int a = 10, b = 20, x = 5, y = 2;
-        int f1, f2, f3, f4, f5, f6, f7, f8, f9, f10;
-        f1 = a + b;
-        f2 = x - b;
-        f3 = x * b;
-        f4 = y / a;
-        f5 = b % y;
-        f6 = y + 10 % x;
-        f7 = a / 3 * 5;
-        f8 = 9 / 2 * a;
-        f9 = y % 8;
-        f10 = 100 * x + y % 2 - a;
-        Console.WriteLine("result :\n1={0}\n2={1}\n3={2}\n4={3}\n5={4}\n6={5}\n7={6}\n8={7}\n9={8}\n10={9}", f1, f2, f3, f4, f5, f6, f7, f8, f9, f10);
+        static void Main(string[] args)
+        {
+            const double lightSpeed = 186000d;   // miles per second
+            Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
+            const double mileTokm = 1.609344;
+            Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
+            const double SunToEarthDistance = 93000000d;  // miles
+            Console.WriteLine("SunToEarthDistance = {0} km", SunToEarthDistance * mileTokm);
+            double SunToEarthTimeOfLight = SunToEarthDistance / lightSpeed;  // miles
+            Console.WriteLine("SunToEarthTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
+            Console.WriteLine("SunToEarthTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
+        }
     }
 }
