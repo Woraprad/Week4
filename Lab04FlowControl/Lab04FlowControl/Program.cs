782b13befd12ee5aa287f5bcd89b0884ddec536d
Woraprad <58030217@kmitl.ac.th>
2017-10-29 15:42:59 +0700

    1.1.2.test3

--- Lab04FlowControl/Lab04FlowControl/Program.cs
+++ Lab04FlowControl/Lab04FlowControl/Program.cs
@@ -3,19 +3,19 @@ public class TryCatch
 {
     static void Main(string[] args)
     {
-        int a = 0;
-        int b = 10;
-        
+        int value = 800000000;
         try
         {
-            b /= a;
-            Console.WriteLine(a);
+            checked // check for overflow
+            {
+            int square = value * value;
+            Console.WriteLine("{0} ^ 2 = {1}", value, square);
+            }
         }
-        catch (DivideByZeroException e)
+        catch (OverflowException e)
         {
             Console.WriteLine(e.Message);
         }
-
 
     }
 }
